03c4316a47716d1b7a65a8f010ecc0341c6a23a2
Mona Lisa <kotzmax@gmail.com>
2024-10-03 20:28:46 +0200

    added some classes

--- spike/__init__.py
+++ spike/__init__.py
@@ -1,26 +1,19 @@
-# __init__.py
-
 from .app import App
-from .primehub import PrimeHub
+from .primehub import PrimeHub, Button, Speaker, LightMatrix, StatusLight, MotionSensor
 from .motor import Motor
 from .motorpair import MotorPair
 from .colorsensor import ColorSensor
-from .math import (
-    acos, acosh, asin, asinh, atan, atan2, atanh, ceil, copysign, cos, cosh,
-    degrees, erf, erfc, exp, expm1, fabs, floor, fmod, frexp, gamma, isfinite,
-    isinf, isnan, ldexp, lgamma, log, log10, log2, modf, pow, radians, sin,
-    sinh, sqrt, tan, tanh, trunc, e, pi
-)
+
 
 __all__ = [
     'App',
     'PrimeHub',
+    'Button',
+    'Speaker',
+    'LightMatrix',
+    'StatusLight',
+    'MotionSensor',
     'Motor',
     'MotorPair',
     'ColorSensor',
-    'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 'ceil',
-    'copysign', 'cos', 'cosh', 'degrees', 'erf', 'erfc', 'exp', 'expm1',
-    'fabs', 'floor', 'fmod', 'frexp', 'gamma', 'isfinite', 'isinf', 'isnan',
-    'ldexp', 'lgamma', 'log', 'log10', 'log2', 'modf', 'pow', 'radians',
-    'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'trunc', 'e', 'pi'
 ]
